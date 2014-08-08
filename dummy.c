6bf5530c9abf5c909b0096dfb41ad0c7912791b9
Andy Grover <agrover@redhat.com>
2014-08-08 14:56:58 -0700

    dummy: Implement file-backed read support
    Open test.img, and save fd to hm_private.
    
    Get the length and the lba from the cdb.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- dummy.c
+++ dummy.c
@@ -2,21 +2,49 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <endian.h>
 
 #include "../kernel/drivers/target/target_core_user.h"
 #include "tcmu-runner.h"
 
 int block_size = 4096;
 
+struct dummy_state {
+	int fd;
+};
+
 int dummy_open(struct tcmu_device *dev)
 {
+	struct dummy_state *state;
+
+	state = calloc(1, sizeof(*state));
+	if (!state)
+		return -1;
+
+	dev->hm_private = state;
+
+	state->fd = open("test.img", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (state->fd == -1) {
+		printf("could not open: %m\n");
+		return -1;
+	}
+
 	return 0;
 }
 
 void dummy_close(struct tcmu_device *dev)
 {
+	struct dummy_state *state;
+
+	state = dev->hm_private;
+
+	close(state->fd);
 }
 
 /*
@@ -25,6 +53,10 @@ void dummy_close(struct tcmu_device *dev)
 bool dummy_cmd_submit(struct tcmu_device *dev, uint8_t *cdb, struct iovec *iovec)
 {
 	uint8_t cmd;
+	void *buf;
+	struct dummy_state *state;
+
+	state = dev->hm_private;
 
 	printf("handling a command!\n");
 
@@ -34,25 +66,53 @@ bool dummy_cmd_submit(struct tcmu_device *dev, uint8_t *cdb, struct iovec *iovec
 
 	if (cmd == 0x28) { // READ 10
 		int i;
-		int data_bytes;
 		int remaining;
+		int lba = be32toh(*((u_int32_t *)&cdb[2]));
+		int length = be16toh(*((uint16_t *)&cdb[7])) * block_size;
+		off_t ret;
+		void *tmp_ptr;
 
 		for (i = 0; i < 10; i++) {
 			printf("%x ", cdb[i]);
 		}
 		printf("\n");
+		printf("lba %d length %d\n", lba, length);
 
-		/* clients must lookup block size from configfs */
-		data_bytes = block_size * cdb[8];
+		buf = malloc(length);
+		if (!buf)
+			return false;
+		memset(buf, 0, length);
 
-		remaining = data_bytes;
+		ret = lseek(state->fd, lba*block_size, SEEK_SET);
+		if (ret == -1) {
+			printf("lseek failed: %m\n");
+			free(buf);
+			return false;
+		}
+
+		ret = read(state->fd, buf, length);
+		if (ret == -1) {
+			printf("read failed: %m\n");
+			free(buf);
+			return false;
+		}
+
+		remaining = length;
+		tmp_ptr = buf;
 
 		while (remaining) {
-			memset(iovec->iov_base, 0, iovec->iov_len);
+			unsigned int to_copy;
 
+			to_copy = (remaining > iovec->iov_len) ? iovec->iov_len : remaining;
+
+			memcpy(iovec->iov_base, tmp_ptr, to_copy);
+
+			tmp_ptr += to_copy;
 			remaining -= iovec->iov_len;
 			iovec++;
 		}
+
+		free(buf);
 
 		return true;
 	}
